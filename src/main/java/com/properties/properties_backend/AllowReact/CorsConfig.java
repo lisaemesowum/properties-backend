02142c309a45b5555c4305aca319db6ca3f9036d
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2025-11-21 00:06:32 +0100

    Update CorsConfig.java

--- src/main/java/com/properties/properties_backend/AllowReact/CorsConfig.java
+++ src/main/java/com/properties/properties_backend/AllowReact/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.properties.properties_backend.AllowReact;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,14 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.url}")
+    private  String frontendUrl;
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
 
-                registry.addMapping("/**")
-                        .allowedOrigins("https://properties-management-system.vercel.app") // React port
+                registry.addMapping("/api/**")
+                        .allowedOriginPatterns("*", "https://properties-management-system-l2d9.vercel.app")     // ← THIS IS THE FIX (allows localhost:5173, vercel.app, etc.)
+//                        .allowedOrigins("https://properties-management-system.vercel.app") // React port
+//                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
